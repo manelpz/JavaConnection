6a17f2308bf1f9c42d059817e5e3b92aa4c86661
Emmanuel Lopez Guerrero <manelpz@icloud.com>
2026-02-24 21:09:50 -0600

    creation class MySqlCRUD

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+
+        Customer c1 = new Customer(1, "emma", "lopez", 36, "emma.doe@example.com");
+        Customer c2 = new Customer(2, "juanito", "perez", 26, "juanito.perez@example.com");
+
+        MySqlCRUD conn = new MySqlCRUD();
+        
+
+
+        
     }
 }
